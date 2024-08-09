308cfc433b23afb6e5a9bb7fac3a97410cfc6379
Diego A dos Santos <diegoaugusto01@gmail.com>
2024-08-09 10:45:41 -0300

    Criando a classe Acessorio

---
+++ src/main/java/edu/ifsp/dwi/modelo/Acessorio.java
@@ -0,0 +1,25 @@
+package edu.ifsp.dwi.modelo;
+
+public class Acessorio {
+	
+	private String descricao;
+	private Double preco;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
