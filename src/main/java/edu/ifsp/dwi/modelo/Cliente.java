3bdfffded82513a1fa3ca972077f1562d181d248
Diego A dos Santos <diegoaugusto01@gmail.com>
2024-08-09 10:40:09 -0300

    Criando a pasta resources e adicionando os arquivos data.sql e schema.sql, criando a classe Cliente

---
+++ src/main/java/edu/ifsp/dwi/modelo/Cliente.java
@@ -0,0 +1,43 @@
+package edu.ifsp.dwi.modelo;
+
+public class Cliente {
+	
+	private String nome;
+	private String email;
+	private String endereço;
+	private String username;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEndereço() {
+		return endereço;
+	}
+	
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+}
