b85381e573074053d22160d31ef5b2a102ea62a5
Diego A dos Santos <diegoaugusto01@gmail.com>
2024-08-09 10:50:23 -0300

    Criando a classe Jogo

---
+++ src/main/java/edu/ifsp/dwi/modelo/Jogo.java
@@ -0,0 +1,52 @@
+package edu.ifsp.dwi.modelo;
+
+public class Jogo {
+
+	private String nome;
+	private String midia;
+	private Double preco;
+	private String categoria;
+	private String plataforma;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getMidia() {
+		return midia;
+	}
+	
+	public void setMidia(String midia) {
+		this.midia = midia;
+	}
+	
+	public Double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public String getPlataforma() {
+		return plataforma;
+	}
+	
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+	
+	
+}
