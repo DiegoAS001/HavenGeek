a3f6269a519a202f326f467e70b026cddb1e0a18
Diego A. dos Santos <diegoaugusto001@gmail.com>
2024-08-13 10:42:54 -0300

    Criando a classe DatabaseConnector (banco de dados implementado foi o H2 database)

---
+++ src/main/java/edu/ifsp/dwi/persistencia/DatabaseConnector.java
@@ -0,0 +1,25 @@
+package edu.ifsp.dwi.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+	
+	 private static final String JDBC_URL = "jdbc:h2:~/HavenGeek"; 
+	    private static final String USER = "sa";
+	    private static final String PASSWORD = "";
+
+	    public static Connection connect() {
+	        Connection connection = null;
+	        try {
+	            connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+	            System.out.println("Conexão estabelecida com sucesso!");
+	        } catch (SQLException e) {
+	            System.out.println("Erro ao conectar ao banco de dados");
+	            e.printStackTrace();
+	        }
+	        return connection;
+	    }
+
+}
