8fd804f0a2c0e6ed0c633d101251c97d1bf19233
Diego A. dos Santos <diegoaugusto001@gmail.com>
2024-08-14 11:05:39 -0300

    Criando o Servlet InicioServlet

---
+++ src/main/java/edu/ifsp/dwi/web/cliente/InicioServlet.java
@@ -0,0 +1,29 @@
+package edu.ifsp.dwi.web.cliente;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.ifsp.web.templates.Template;
+
+@WebServlet("/")
+public class InicioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Template.render("inicio/paginaInicial", request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
