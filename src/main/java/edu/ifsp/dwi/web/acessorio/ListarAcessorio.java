43fd3ebfbd70557cab463a0b04836f2a2c8e416e
Diego A. dos Santos <diegoaugusto001@gmail.com>
2024-08-16 11:22:53 -0300

    Inicio da implementação controller

--- src/main/java/edu/ifsp/dwi/web/acessorio/ListarAcessorio.java
+++ src/main/java/edu/ifsp/dwi/web/acessorio/ListarAcessorio.java
@@ -1,30 +1,28 @@
 package edu.ifsp.dwi.web.acessorio;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.ifsp.dwi.modelo.Acessorio;
 import edu.ifsp.dwi.persistencia.AcessorioDAO;
 import edu.ifsp.dwi.web.templates.Template;
+import edu.ifsp.dwi.web.Command;
 
-@WebServlet("/ListarAcessorio")
-public class ListarAcessorio extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		Template.render("acessorio/listarAcessorio", request, response);
-	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-			AcessorioDAO dao = new AcessorioDAO();
-			
+public class ListarAcessorio implements Command {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		AcessorioDAO dao = new AcessorioDAO();
+		List<Acessorio> acessorios = dao.listarTodos();
+		request.setAttribute("acessorios", acessorios);
+		Template.render("produto/listar", request, response);		
 	}
 
 }
