f7c702ebcd6158ebb435c8c483048c1f67dc7f37
Rafael Souza <rafael.godoy@aluno.ifsp.edu.br>
2024-08-22 15:18:15 -0300

    Implementacao da classe RecuperarCliente.

---
+++ src/main/java/edu/ifsp/dwi/web/cliente/RecuperarCliente.java
@@ -0,0 +1,24 @@
+package edu.ifsp.dwi.web.cliente;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ifsp.dwi.modelo.Cliente;
+import edu.ifsp.dwi.persistencia.ClienteDAO;
+import edu.ifsp.dwi.web.Command;
+import edu.ifsp.dwi.web.templates.Template;
+
+public class RecuperarCliente implements Command{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		ClienteDAO dao = new ClienteDAO();
+		Cliente cliente = dao.buscarPeloId(id);
+		request.setAttribute("cleinte", cliente);
+		Template.render("cliente/detalharCliente", request, response);
+		
+	}
+
+}
